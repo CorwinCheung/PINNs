4fdead198af263c03f3d8d3bcc5cd0ad5f8b29fe
Marcos Johnson-Noya <mjohnsonnoya@college.harvard.edu>
2023-08-11 11:51:47 -0600

    Marcos updates, getting graph, but looks like no training data

--- appendix/continuous_time_inference (Burgers)/Burgers.py
+++ appendix/continuous_time_inference (Burgers)/Burgers.py
@@ -6,6 +6,7 @@ import sys
 sys.path.insert(0, '../../Utilities/')
 
 import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
@@ -18,7 +19,7 @@ import matplotlib.gridspec as gridspec
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 np.random.seed(1234)
-tf.set_random_seed(1234)
+tf.compat.v1.set_random_seed(1234)
 
 class PhysicsInformedNN:
     # Initialize the class
@@ -42,15 +43,15 @@ class PhysicsInformedNN:
         self.weights, self.biases = self.initialize_NN(layers)
         
         # tf placeholders and graph
-        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+        self.sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(allow_soft_placement=True,
                                                      log_device_placement=True))
         
-        self.x_u_tf = tf.placeholder(tf.float32, shape=[None, self.x_u.shape[1]])
-        self.t_u_tf = tf.placeholder(tf.float32, shape=[None, self.t_u.shape[1]])        
-        self.u_tf = tf.placeholder(tf.float32, shape=[None, self.u.shape[1]])
+        self.x_u_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, self.x_u.shape[1]])
+        self.t_u_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, self.t_u.shape[1]])        
+        self.u_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, self.u.shape[1]])
         
-        self.x_f_tf = tf.placeholder(tf.float32, shape=[None, self.x_f.shape[1]])
-        self.t_f_tf = tf.placeholder(tf.float32, shape=[None, self.t_f.shape[1]])        
+        self.x_f_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, self.x_f.shape[1]])
+        self.t_f_tf = tf.compat.v1.placeholder(tf.float32, shape=[None, self.t_f.shape[1]])        
                 
         self.u_pred = self.net_u(self.x_u_tf, self.t_u_tf) 
         self.f_pred = self.net_f(self.x_f_tf, self.t_f_tf)         
@@ -59,15 +60,15 @@ class PhysicsInformedNN:
                     tf.reduce_mean(tf.square(self.f_pred))
                
                 
-        self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.loss, 
-                                                                method = 'L-BFGS-B', 
-                                                                options = {'maxiter': 50000,
-                                                                           'maxfun': 50000,
-                                                                           'maxcor': 50,
-                                                                           'maxls': 50,
-                                                                           'ftol' : 1.0 * np.finfo(float).eps})
-        
-        init = tf.global_variables_initializer()
+        # self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.loss, 
+        #                                                         method = 'L-BFGS-B', 
+        #                                                         options = {'maxiter': 50000,
+        #                                                                    'maxfun': 50000,
+        #                                                                    'maxcor': 50,
+        #                                                                    'maxls': 50,
+        #                                                                    'ftol' : 1.0 * np.finfo(float).eps})
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+        init = tf.compat.v1.global_variables_initializer()
         self.sess.run(init)
 
                 
@@ -86,7 +87,7 @@ class PhysicsInformedNN:
         in_dim = size[0]
         out_dim = size[1]        
         xavier_stddev = np.sqrt(2/(in_dim + out_dim))
-        return tf.Variable(tf.truncated_normal([in_dim, out_dim], stddev=xavier_stddev), dtype=tf.float32)
+        return tf.Variable(tf.random.truncated_normal([in_dim, out_dim], stddev=xavier_stddev), dtype=tf.float32)
     
     def neural_net(self, X, weights, biases):
         num_layers = len(weights) + 1
@@ -122,10 +123,10 @@ class PhysicsInformedNN:
         tf_dict = {self.x_u_tf: self.x_u, self.t_u_tf: self.t_u, self.u_tf: self.u,
                    self.x_f_tf: self.x_f, self.t_f_tf: self.t_f}
                                                                                                                           
-        self.optimizer.minimize(self.sess, 
-                                feed_dict = tf_dict,         
-                                fetches = [self.loss], 
-                                loss_callback = self.callback)        
+        # self.optimizer.minimize(self.sess, 
+        #                         feed_dict = tf_dict,         
+        #                         fetches = [self.loss], 
+        #                         loss_callback = self.callback)        
                                     
     
     def predict(self, X_star):
